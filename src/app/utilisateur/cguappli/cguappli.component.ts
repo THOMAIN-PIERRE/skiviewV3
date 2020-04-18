bf0c598602c5edd9ceddf0f10445263e0b65fd40
mathieuh64400 <mhedot@neuf.fr>
2020-04-18 09:41:29 +0200

    sauvegarde après ajout de mes données HTML

---
+++ src/app/utilisateur/cguappli/cguappli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-cguappli',
+  templateUrl: './cguappli.component.html',
+  styleUrls: ['./cguappli.component.css']
+})
+export class CguappliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
